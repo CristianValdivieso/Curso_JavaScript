cbc70f369be0b99bc4bda25744d7a571310e2ee6
Cristian Valdivieso <cristian.valdivieso@unesp.br>
2020-06-28 13:08:02 -0300

    Class 11 added
    Subí la clase 11 al curso de JS

---
+++ Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`La velocidad del carro es ${vel} km/h`)
+if (vel > 60){
+    console.log(`Usted ultrapasó la velocidad permitida. Usted está MULTADO!`)
+}
+console.log(`Dirija siempre con cinturón de seguridad!`)
---
+++ Aula 11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Viviendo en ${pais}`)
+if(pais == 'Brasil'){
+    console.log('Usted es brasileno')
+} else{
+    console.log('Usted es extranjero')
+}
