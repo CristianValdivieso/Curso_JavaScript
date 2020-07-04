d888164eb917d0ecc0b683a226cab2bfa4a5d3b2
Cristian Valdivieso <cristian.valdivieso@unesp.br>
2020-07-04 20:03:12 -0300

    Class 13 added

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,22 @@
+/*var c = 1
+while (c <= 3){
+    console.log(`Paso ${c}`)
+    c++ 
+}*/
+
+var c = 1
+do {
+    console.log(`Paso ${c}`)
+    c++ 
+} while (c <= 5)
+
+
+
+/*console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')*/
